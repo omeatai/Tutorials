f1084d46c0aa42990333a6f7ff7cd2a0b7efd5eb
IFEANYI OMEATA <32337103+omeatai@users.noreply.github.com>
2021-09-08 20:48:04 +0000

    m

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let cells = document.getElementsByClassName("cell");
 console.log(cells[1]);
 cells[2].innerHTML = "my New Mango";
 cells[2].style.fontWeight = 700;
+
+for(let i = 0; i < cells.length; i++){
+  cells[i].style.backgroundColor = "f0f0f0";
+}
