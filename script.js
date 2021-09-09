ff27437b0430d61e2b15db1075112fb84747c5c7
IFEANYI OMEATA <32337103+omeatai@users.noreply.github.com>
2021-09-09 08:12:30 +0000

    m

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
-let myHeader = document.querySelector("#my-header");
-let myForm = document.querySelector("form");
-let myInput = document.querySelector("input");
-let submitButton = document.querySelector("input[type='submit']");
-let lastChildOfList = document.querySelector(".cell:last-child");
-let secondChildOfList = document.querySelector(".cell:nth-child(2)");
+let myPTagsAll = document.querySelectorAll("p");
+console.log(myPTagsAll);
 
-console.log(myHeader);
-console.log(myForm);
-console.log(myInput);
-console.log(submitButton);
-
-myHeader.innerHTML = "my Query header";
-myHeader.style.borderBottom = '20px #ccc solid';
-myForm.style.backgroundColor = '#ccc';
-myForm.style.border = '10px #ccc solid';
-myInput.value = "my sample Text";
-submitButton.value = "ENTER";
-lastChildOfList.style.color = 'blue';
-secondChildOfList.style.color = 'red';
+for(let i = 0; i < myPTagsAll.length; i++){
+  myPTagsAll[i].style.backgroundColor = "#84a98c";
+  myPTagsAll[i].innerHTML = "This is changed!";
+}
 
 
-// for(let i = 0; i < li.length; i++){
-//   li[i].style.backgroundColor = "#84a98c";
-// }
+// let myHeader = document.querySelector("#my-header");
+// let myForm = document.querySelector("form");
+// let myInput = document.querySelector("input");
+// let submitButton = document.querySelector("input[type='submit']");
+// let lastChildOfList = document.querySelector(".cell:last-child");
+// let secondChildOfList = document.querySelector(".cell:nth-child(2)");
+
+// console.log(myHeader);
+// console.log(myForm);
+// console.log(myInput);
+// console.log(submitButton);
 
 
 
-// cells[2].innerHTML = "my New Mango";
-// cells[2].style.fontWeight = 700;
+// myHeader.innerHTML = "my Query header";
+// myHeader.style.borderBottom = '20px #ccc solid';
+// myForm.style.backgroundColor = '#ccc';
+// myForm.style.border = '10px #ccc solid';
+// myInput.value = "my sample Text";
+// submitButton.value = "ENTER";
+// lastChildOfList.style.color = 'blue';
+// secondChildOfList.style.color = 'red';
 
-// for(let i = 0; i < cells.length; i++){
-//   cells[i].style.backgroundColor = "#84a98c";
-// }
+
+
+
+
+
+// // cells[2].innerHTML = "my New Mango";
+// // cells[2].style.fontWeight = 700;
+
+// // for(let i = 0; i < cells.length; i++){
+// //   cells[i].style.backgroundColor = "#84a98c";
+// // }
