df2c3c0874f427bca95065f8e91dea3aceadfb23
IFEANYI OMEATA <32337103+omeatai@users.noreply.github.com>
2021-10-04 17:40:48 +0000

    m

--- answers.js
+++ answers.js
@@ -102,6 +102,12 @@
 //######################
 // 1.Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
 
+function sum(arr, n) {
+  let result = 0;
+  for(let i = 0; i < arr.length; i++){
+    
+  }
+}
 
 
 
