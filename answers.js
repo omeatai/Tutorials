807facc9eb12e90efa072d9b352f19cd918511e7
IFEANYI OMEATA <32337103+omeatai@users.noreply.github.com>
2021-10-04 17:31:06 +0000

    m

--- answers.js
+++ answers.js
@@ -111,7 +111,6 @@
 //   ourArray.push(i);
 //   i++;
 // }while(i < 5);
-
 // console.log(ourArray);
 
 // 2.Change the while loop in the code to a do...while loop so the loop will push only the number 10 to myArray, and it will be equal to 11 when the code has finished running.
@@ -123,14 +122,12 @@
 //   i++;
 // }
 
-
 let myArray = [];
 let i = 10;
-while (i < 10) {
+do{
   myArray.push(i);
   i++;
-}
-
+}while (i < 10);
 
 
 
